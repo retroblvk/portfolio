d91d1ebd65de4401ba5b0a13e9d55621d4ba20f8
retro <retroblvk@gmail.com>
2021-05-26 14:40:08 -0600

    removed .env, and packages and adjusted portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,76 +1,47 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+
 export default function Portfolio() {
+  const [projects, setProjects] = useState(null);
+  useEffect(() => {
+    document.title = 'retroblvk | Portfolio';
+    Axios.get('http://localhost:1337/projects')
+      .then((response) => {
+        console.log(response.data);
+        setProjects(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <main className='container mx-auto px-8 py-2'>
       <h1 className='text-4xl font-bold'>Portfolio</h1>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-1 md:gap-4 pt-8'>
-        <div className='card mb-4'>
-          <img
-            src='https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-            alt=''
-            className='h-50 w-full mb-2'
-          />
-          <h1>Some Project</h1>
-          <ul className='flex flex-wrap tags font-bold'>
-            <li className='mr-2'>Personal</li>
-            <li className='mr-2'>React</li>
-            <li className='mr-2'>Strapi</li>
-          </ul>
-          <ul className='flex'>
-            <li className='mr-2'>Github</li>
-            <li>Site</li>
-          </ul>
-        </div>
-        <div className='card mb-4'>
-          <img
-            src='https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-            alt=''
-            className='h-50 w-full mb-2'
-          />
-          <h1>Some Project</h1>
-          <ul className='flex flex-wrap tags font-bold'>
-            <li className='mr-2'>Personal</li>
-            <li className='mr-2'>React</li>
-            <li className='mr-2'>Strapi</li>
-          </ul>
-          <ul className='flex'>
-            <li className='mr-2'>Github</li>
-            <li>Site</li>
-          </ul>
-        </div>
-        <div className='card mb-4'>
-          <img
-            src='https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-            alt=''
-            className='h-50 w-full mb-2'
-          />
-          <h1>Some Project</h1>
-          <ul className='flex flex-wrap tags font-bold'>
-            <li className='mr-2'>Personal</li>
-            <li className='mr-2'>React</li>
-            <li className='mr-2'>Strapi</li>
-          </ul>
-          <ul className='flex'>
-            <li className='mr-2'>Github</li>
-            <li>Site</li>
-          </ul>
-        </div>
-        <div className='card mb-4'>
-          <img
-            src='https://images.unsplash.com/photo-1522542550221-31fd19575a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
-            alt=''
-            className='h-50 w-full mb-2'
-          />
-          <h1>Some Project</h1>
-          <ul className='flex flex-wrap tags font-bold'>
-            <li className='mr-2'>Personal</li>
-            <li className='mr-2'>React</li>
-            <li className='mr-2'>Strapi</li>
-          </ul>
-          <ul className='flex'>
-            <li className='mr-2'>Github</li>
-            <li>Site</li>
-          </ul>
-        </div>
+        {projects &&
+          projects.map((project, index) => (
+            <div className='card' key={index}>
+              <img
+                src={
+                  'http://localhost:1337' +
+                  project.projectImage[0].formats.medium.url
+                }
+                alt=''
+                className='mb-2'
+              />
+              <h1 className='text-lg  mb-4'>{project.title}</h1>
+              <ul className='flex'>
+                {project.tags &&
+                  project.tags.map((tag, tagIndex) => {
+                    return (
+                      <li key={tagIndex} className='mr-2'>
+                        {tag.name}
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          ))}
       </div>
     </main>
   );
