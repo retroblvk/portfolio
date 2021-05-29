54dea44bc32cec7fbf16b0ffd06c318fab8fbe58
retro <retroblvk@gmail.com>
2021-05-28 18:37:57 -0600

    Updated strapi url

--- src/helper.js
+++ src/helper.js
@@ -2,7 +2,7 @@ let strapi_url = null;
 if (process.env.NODE_ENV === 'development') {
   strapi_url = 'http://localhost:1337';
 } else {
-  strapi_url = 'https://frozen-falls-28617.herokuapp.com';
+  strapi_url = 'https://retroblvk-portfolio-strapi.herokuapp.com/';
 }
 
 export default strapi_url;
